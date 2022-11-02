d967cd0727c08186883f447017bbb27fbb93bf80
malefikus13 <tverdokhlebov13@gmail.com>
2022-11-02 18:08:58 +0300

    homework 8

---
+++ src/public/js/ProducComp.js
@@ -0,0 +1,49 @@
+const product = {
+	props: ['product', 'img'],
+	template: `
+	<div class="product-item">
+	<img :src="img" alt="img">
+	<div class="desc">
+			<h3>{{product.product_name}}</h3>
+			<p>{{product.price}} руб.</p>
+			<button class="buy-btn" @click="$root.$refs.cart.addProduct(product)">Купить</button>
+	</div>
+</div>
+    `
+};
+
+const products = {
+	data() {
+		return {
+			catalogUrl: '/catalogData.json',
+			products: [],
+			filtered: [],
+			imgCatalog: 'https://via.placeholder.com/150/007fcd/ffffff/?text=Image',
+		}
+	},
+	components: {
+		product
+	},
+	methods: {
+		filter(value) {
+			let regexp = new RegExp(value, 'i');
+			this.filtered = this.products.filter(el => regexp.test(el.product_name));
+		}
+	},
+	mounted() {
+		this.$parent.getJson(`/api/products`)
+			.then(data => {
+				for (let el of data) {
+					this.products.push(el);
+					this.filtered.push(el);
+				}
+			});
+	},
+	template: `
+	<div class="products">
+	<product v-for="item of filtered" :key="item.id_product" :img="imgCatalog" :product="item"></product>
+</div>
+    `
+};
+
+export default products
